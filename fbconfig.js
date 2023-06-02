cc98e621e524decdfa1c1c966215f9416a605094
Caetanocc <58012552+Caetanocc@users.noreply.github.com>
2023-06-01 22:18:07 -0300

    Update fbconfig.js no credentials

--- fbconfig.js
+++ fbconfig.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyC4IpXHz4wbwb5emaPFoVpz51gQvkDoF2U",
-    authDomain: "etec22s2.firebaseapp.com",
-    databaseURL: "https://etec22s2-default-rtdb.firebaseio.com",
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
     projectId: "etec22s2",
-    storageBucket: "etec22s2.appspot.com",
-    messagingSenderId: "429631871423",
-    appId: "1:429631871423:web:8a91da2ae6bb57f88fa90a",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "1",
     measurementId: "G-LZS9HLK6PQ"
     };
   
